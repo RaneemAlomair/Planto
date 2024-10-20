1411d5087c0e6c5c1f11d154acf3103b96d9276a
RaneemAlomair <raneemalomair11@gmail.com>
2024-10-20 17:12:12 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Raneem Alomair on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
